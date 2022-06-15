1eada70321893e8c046ca70d690314770bd137b1
livelliott <96855754+livelliott@users.noreply.github.com>
2022-06-15 15:04:02 -0400

    Began Working on Responsive Nav Bar

---
+++ myapp.js
@@ -0,0 +1,10 @@
+// const navSlide = () => {
+//     const burger = document.querySelector(".burger");
+//     const nav = document.querySelector(".links")
+
+//     // when burger is clicked 
+//     burger.addEventListener('click', () => {
+//         // active nav bar
+//         nav.classList.toggle('.nav-active');
+//     });
+// }
