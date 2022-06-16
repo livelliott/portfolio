8fd8138f4ffd6e83a440d3d7ab3b8deb1d52d684
livelliott <96855754+livelliott@users.noreply.github.com>
2022-06-16 03:01:58 -0400

    Functional Nav Bar with Transitions

--- myapp.js
+++ myapp.js
@@ -1,10 +1,27 @@
-// const navSlide = () => {
-//     const burger = document.querySelector(".burger");
-//     const nav = document.querySelector(".links")
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".links");
+    const navLinks = document.querySelector(".links li");
 
-//     // when burger is clicked 
-//     burger.addEventListener('click', () => {
-//         // active nav bar
-//         nav.classList.toggle('.nav-active');
-//     });
-// }
+    // when burger is clicked 
+    burger.addEventListener('click', ()=>{
+        // active nav bar
+        // nav.classList.toggle('nav-active');
+
+        if (nav.classList.contains('nav-inactive')) {
+            nav.classList.remove('nav-inactive');
+            nav.classList.add('nav-active');
+
+        }
+        else {
+            nav.classList.remove('nav-active');
+            nav.classList.add('nav-inactive');
+        }
+
+        // burger animation: when we click on the burger -> form an X shape
+        burger.classList.toggle('toggle'); 
+
+    });
+}
+
+navSlide();
