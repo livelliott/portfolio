7fd3b717d10a4601d59668168d0ac806f4bf853f
livelliott <96855754+livelliott@users.noreply.github.com>
2022-07-10 01:05:39 -0400

    Modify Nav Bar

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".nav-menu");
+const nav = document.querySelector("nav");
+const contains = document.querySelector(".container");
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
+    nav.classList.toggle('no-blur');
+    nav.classList.toggle('full-opacity');
+    // contains.classList.toggle('full-blur');
+
 })
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
